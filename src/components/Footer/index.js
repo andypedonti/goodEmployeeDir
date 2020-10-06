a466894e015660a716e07198dd987b7b424defc8
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-10-06 17:36:32 -0400

    last commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <h1 className="coolFooter">This is the coolest footer you have ever seen</h1>
+            <h1 className="coolFooter">Employee Directory</h1>
         </div>
     )
 }
